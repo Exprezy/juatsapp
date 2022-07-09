fc77ab23acbb6809f4c30e6848ab234b7624091f
Exprezy <107974064+Exprezy@users.noreply.github.com>
2022-07-08 20:50:52 -0700

    Primera vez

---
+++ JuatsappNegocio/src/controles/ControlDirecciones.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controles;
+
+import entidades.Direccion;
+import entidades.Usuario;
+import interfaces.IDatos;
+import java.util.List;
+
+/**
+ *
+ * @author focod
+ */
+public class ControlDirecciones {
+    private IDatos datos;
+    
+    protected ControlDirecciones(IDatos datos){
+        this.datos = datos;
+    }
+    
+    public boolean guardarDireccion(Direccion direccion){
+        return datos.guardarDireccion(direccion);
+    }
+    
+    public boolean eliminarDireccion(Direccion direccion) {
+        return datos.eliminarDireccion(direccion);
+    }
+    
+}
