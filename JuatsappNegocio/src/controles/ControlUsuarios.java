48aa8dbcf2645fe51d635624949d7c5abbb8e40c
Exprezy <107974064+Exprezy@users.noreply.github.com>
2022-07-09 21:27:22 -0700

    agregue control

--- JuatsappNegocio/src/controles/ControlUsuarios.java
+++ JuatsappNegocio/src/controles/ControlUsuarios.java
@@ -14,6 +14,7 @@ import interfaces.IDatos;
 public class ControlUsuarios {
 
     private IDatos datos;
+    String control;
     
     protected ControlUsuarios(IDatos datos){
         this.datos = datos;
